53270a30bf1e69098fbe813e14d6846f9b65e5cc
Br0ckers <neil.brockman@yahoo.co.uk>
2018-10-04 16:41:03 +0100

    Added array for bike inventory

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,20 +1,30 @@
 class DockingStation
-  attr_reader :bike
+  #attr_reader :bike
+
+  def initialize
+    @bike_inv = []
+    @max_val = 20
+  end
 
   def release_bike
     #raise "No bikes" if @bike.nil?
-    fail "No bikes" if @bike.nil?
-    @bike
+    fail "No bikes" if @bike_inv.length == 0
+    @bike_inv.pop
   end
 
   def dock(bike)
 
-    raise "Station full" unless @bike.nil?
+    raise "Station full" unless @bike_inv.length < 20
     #unless @bike.nil? raise "Station full"
-    @bike = bike
+    @bike_inv << bike
+    return "dock success"
     #@bikes << bike
   end
 
+  def available_bikes
+    @bike_inv.length
+  end
+  
 end
 
 
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,25 +4,26 @@ describe DockingStation do
     it {is_expected.to respond_to :release_bike}
 
     it 'shows bikes in dock' do
+      dock_cap = subject.available_bikes
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.available_bikes).to eq dock_cap + 1
     end
 
     it { is_expected.to respond_to(:dock).with(1).argument }
 
     it 'docks the bike' do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq "dock success"
     end
 
-    it 'returns the docked bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.bike).to eq bike
-    end
+    #it 'confirms the bike is docked' do
+    #  bike = Bike.new
+    #  subject.dock(bike)
+    #  expect(subject.bike).to eq "dock success"
+  #  end
 
-    it 'releases the bike put in the dock' do
+    it 'releases the bike in the dock' do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
@@ -37,7 +38,7 @@ describe DockingStation do
     end
 
     it 'if dock at capacity don\'t accept more and Error' do
-      expect{20.times {subject.dock(Bike.new)}}.to raise_error "Station full"
+      expect{21.times {subject.dock(Bike.new)}}.to raise_error "Station full"
     end
 
 
